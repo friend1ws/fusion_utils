b5b3b885aeefa732a664e125707be7f8394220dd
friend1ws <friend1ws@gmail.com>
2015-09-29 17:34:54 +0900

    modification by changing the package name

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@
 
 from distutils.core import setup
 
-setup(name='fusion_comp',
+setup(name='fusion_utils',
       version='0.1.0',
       description='Python tools for comparing gene fusion and structural variation results/',
       author='Yuichi Shiraishi',
       author_email='friend1ws@gamil.com',
-      url='https://github.com/friend1ws/fusion_comp',
+      url='https://github.com/friend1ws/fusion_utils',
       package_dir = {'': 'lib'},
-      packages=['fusion_comp'],
-      scripts=['fusion_comp'],
+      packages=['fusion_utils'],
+      scripts=['fusion_utils'],
       license='GPL-3'
      )
 
